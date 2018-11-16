657cbc0268d4d00f8ad597d312910875826e5690
J <joydxl@yahoo.com>
2018-11-16 21:56:25 +0200

    managerCanActivate update - redirect if false

--- bookshop/src/app/shared/manager-routing.guard.ts
+++ bookshop/src/app/shared/manager-routing.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from './services/user.service';
+import { NavigationService } from './services/navigation.service';
 
 export interface ComponentCanDeactivate {
   canDeactivate: () => boolean | Observable<boolean>;
@@ -11,12 +12,17 @@ export interface ComponentCanDeactivate {
   providedIn: 'root'
 })
 export class ManagerRoutingGuard implements CanActivate {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private navigationService: NavigationService) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-      return this.userService.isManager();
+      // return this.userService.isManager();
+      if (!this.userService.isManager()) {
+        this.navigationService.goHome();
+      } else {
+        return true;
+      }
   }
   canDeactivate(component: ComponentCanDeactivate): Observable<boolean> | boolean {
 
